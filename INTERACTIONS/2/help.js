dc9e20e8278cfc42f7daca44bad04168e631fbae
kneves378@gmail.com <kneves378@gmail.com>
2023-01-09 10:51:39 -0300

    help att1

--- INTERACTIONS/2/help.js
+++ INTERACTIONS/2/help.js
@@ -2,5 +2,8 @@ const Discord = require('discord.js');
 const chalk = require('chalk');
 
 module.exports = async(interaction, client) => {
+  
+  
+  var embed = new Discord.MessageEmbed()
   interaction.reply({content: "Work!"})
 }
